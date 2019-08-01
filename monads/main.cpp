4bb11ba291df73035fec07320a8f80b03a36beba
Michael Bennett <michaelbennett.dev@gmail.com>
2019-07-31 23:53:55 -0700

    Realized how to properly use my Nullable::then, which is nicer than expected.

--- monads/main.cpp
+++ monads/main.cpp
@@ -4,5 +4,7 @@
 int main()
 {
     Nullable<int> i = 3;
-    i.then([](int n) { std::cout << n << std::endl; });
+    i
+        .then([](int n) { std::cout << n << std::endl; return n; })
+        .then([](int n) { n -= 1; std::cout << n << std::endl; return n; });
 }
